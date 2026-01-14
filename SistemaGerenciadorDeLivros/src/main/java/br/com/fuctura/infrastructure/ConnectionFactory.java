14e5b3011e9f8b51402e59fc4b3f4e05c8133a7b
hebertaragao <hebertaragao.dev@gmail.com>
2026-01-13 21:44:06 -0300

    Update

--- SistemaGerenciadorDeLivros/src/main/java/br/com/fuctura/infrastructure/ConnectionFactory.java
+++ SistemaGerenciadorDeLivros/src/main/java/br/com/fuctura/infrastructure/ConnectionFactory.java
@@ -1,5 +1,26 @@
 package br.com.fuctura.infrastructure;
 
-public class ConnectionFactory {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
-}
+public class ConnectionFactory {
+	
+	
+	public Connection getConnection() {
+		
+		String url = "jdbc:postgresql://fuctura.cy9ok6ouegjq.us-east-1.rds.amazonaws.com:5432/postgres";
+		String user = "aragao";
+		String password = "aragao#aluno";
+		
+		try {
+			Connection c =  DriverManager.getConnection(url, user, password);
+			return c;
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+		return null;
+	}
+		
+};
